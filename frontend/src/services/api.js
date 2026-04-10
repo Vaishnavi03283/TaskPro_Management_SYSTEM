1de8ab805a041d67e1a6c7699506f67983a1bff6
Your Namevaishnavi03283 <you@example.comvaishnavij3112004gmail.com>
2026-04-10 23:13:10 +0530

    Initial Commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL ;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
